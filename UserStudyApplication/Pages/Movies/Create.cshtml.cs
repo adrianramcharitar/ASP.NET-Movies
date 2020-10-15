745d6296e23bc9d563ab24fd996f1f9216e19af5
Adrian Ramcharitar <adrian007760@gmail.com>
2020-10-15 17:25:37 -0400

    init

---
+++ UserStudyApplication/Pages/Movies/Create.cshtml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using RazorPagesMovie.Models;
+using UserStudyApplication.Data;
+
+namespace UserStudyApplication.Pages.Movies
+{
+    public class CreateModel : PageModel
+    {
+        private readonly UserStudyApplication.Data.UserStudyApplicationContext _context;
+
+        public CreateModel(UserStudyApplication.Data.UserStudyApplicationContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Movie Movie { get; set; }
+
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Movie.Add(Movie);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ UserStudyApplication/Pages/Movies/Details.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesMovie.Models;
+using UserStudyApplication.Data;
+
+namespace UserStudyApplication.Pages.Movies
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly UserStudyApplication.Data.UserStudyApplicationContext _context;
+
+        public DetailsModel(UserStudyApplication.Data.UserStudyApplicationContext context)
+        {
+            _context = context;
+        }
+
+        public Movie Movie { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Movie = await _context.Movie.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (Movie == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
